129ecffeec6aa58586aa16b07ad9ae927c2557f4
Yuji Nakakoshi <yuji.nkks@gmail.com>
2023-05-22 14:12:38 +0900

    remove all readable exports

--- exports/image-viewer-preauth-obs-up/readable/application/supporting_objects/deinstall_script.sql
+++
@@ -1,2 +0,0 @@
-drop table img_images;
-drop table img_items;
--- exports/onedrive-operation-odata/readable/application/shared_components/app_static_files/page-actions.js
+++
@@ -1,9 +0,0 @@
-const COPY_BY_BUTTON =
-{
-    name: "copy-by-button",
-    action: function( event, element, args ) {
-        let text = document.getElementById(args.id).textContent;
-        // クリップボードにコピー。
-        navigator.clipboard.writeText(text);
-    }
-};
